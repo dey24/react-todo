fee923b70480f50380878bddd1cb5a3ba669498c
Chiranjit Dey <chiranjitdey2000@gmail.com>
2020-10-19 19:35:38 +0530

    add section

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
-
+import AddTodo from './AddForm';
 class App extends Component{
   state = {
     todos: [
@@ -21,6 +21,7 @@ class App extends Component{
       <div className="todo-app container">
         <h1 className="center blue-text">Todos</h1>
         <Todos todos={this.state.todos} deleteTodo= {this.deleteTodo}/>
+        <AddTodo />
       </div>
     );
   }
--- todoapp/src/Todos.js
+++ todoapp/src/Todos.js
@@ -4,7 +4,7 @@ const Todos = ({todos, deleteTodo}) => {
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
-            <div className="collecion-item" key={todo.id}>
+            <div className="collection-item" key={todo.id}>
                 <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
             </div>
             )
